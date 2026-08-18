ece7953a6c85eb40cf74eafc6892e44a8db72329
Leonardo Marcaccio <leonardo.marcaccio@studio.unibo.it>
2026-08-18 12:02:50 +0200

    feat: A Selector, when asked to assign a mission, should return None if no candidate is available

--- src/test/scala/it/unibo/sentinel/core/selector/SelectorSpec.scala
+++ src/test/scala/it/unibo/sentinel/core/selector/SelectorSpec.scala
@@ -5,6 +5,8 @@ import it.unibo.sentinel.core.robot.Robot
 import it.unibo.sentinel.core.warehouse.Position
 import it.unibo.sentinel.core.selection.Selector
 import it.unibo.sentinel.core.mission.*
+import org.mockito.Mockito
+import org.mockito.Mockito.when
 
 class SelectorSpec extends UnitTest:
 
@@ -22,6 +24,19 @@ class SelectorSpec extends UnitTest:
     "asked to assign a mission" should:
 
       "return None if no candidate is present" in:
-        val result = selector.choose(mission, Iterable.empty[(Robot, Position)])
+        val result = selector.choose(
+          mission,
+          Iterable.empty[(Robot, Position)]
+        )
+
+        result shouldBe None
+
+    "return None if no candidate is available" in:
+        val busy1 = Mockito.mock(classOf[Robot])
+        when(busy1.canAccept).thenReturn(false)
+        
+        val busyRobots = Iterable((busy1, Position(1, 1)))
+
+        val result = selector.choose(mission, busyRobots)
 
         result shouldBe None
