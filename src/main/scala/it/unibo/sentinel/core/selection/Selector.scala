ef2fc26c7e5f05fbd8e25fc1daa908491541ea87
Leonardo Marcaccio <leonardo.marcaccio@studio.unibo.it>
2026-08-18 12:53:39 +0200

    feat: Introduced the concept of Placement for Selector

--- src/main/scala/it/unibo/sentinel/core/selection/Selector.scala
+++ src/main/scala/it/unibo/sentinel/core/selection/Selector.scala
@@ -4,17 +4,23 @@ import it.unibo.sentinel.core.mission.Mission
 import it.unibo.sentinel.core.robot.Robot
 import it.unibo.sentinel.core.warehouse.Position
 
+final case class Placement(
+  robot: Robot,
+  at: Position
+)
+
 object Selector:
 
   def choose(
     mission: Mission,
-    among:Iterable[(Robot, Position)]
-  ): Option[(Robot, Position)] =
+    among:Iterable[Placement]
+  ): Option[Placement] =
     val available = among.filter(_._1.canAccept)
     if available.isEmpty then None
     else selectFromAvailable(mission, available)
 
   def selectFromAvailable(
     mission: Mission,
-    available: Iterable[(Robot, Position)]
-  ): Option[(Robot, Position)] = ???
+    available: Iterable[Placement]
+  ): Option[Placement] = ???
+
--- src/test/scala/it/unibo/sentinel/core/selector/SelectorSpec.scala
+++ src/test/scala/it/unibo/sentinel/core/selector/SelectorSpec.scala
@@ -1,10 +1,13 @@
 package it.unibo.sentinel.core.selector
 
 import it.unibo.sentinel.UnitTest
-import it.unibo.sentinel.core.robot.Robot
+import it.unibo.sentinel.core.robot.*
 import it.unibo.sentinel.core.warehouse.Position
-import it.unibo.sentinel.core.selection.Selector
 import it.unibo.sentinel.core.mission.*
+import it.unibo.sentinel.core.selection.Placement
+import it.unibo.sentinel.core.selection.Selector.*
+import it.unibo.sentinel.core.routing.Navigator
+
 import org.mockito.Mockito
 import org.mockito.Mockito.when
 
@@ -17,7 +20,10 @@ class SelectorSpec extends UnitTest:
     Task.Move(Position(1, 1)),
     duration
   )
-  val selector = Selector
+  
+  val navigator = Mockito.mock(classOf[Navigator])
+  
+  val selector = NearestSelector(navigator)
 
   "A Selector" when:
 
@@ -26,17 +32,32 @@ class SelectorSpec extends UnitTest:
       "return None if no candidate is present" in:
         val result = selector.choose(
           mission,
-          Iterable.empty[(Robot, Position)]
+          Iterable.empty[Placement]
         )
 
         result shouldBe None
 
-    "return None if no candidate is available" in:
+      "return None if no candidate is available" in:
         val busy1 = Mockito.mock(classOf[Robot])
         when(busy1.canAccept).thenReturn(false)
-        
-        val busyRobots = Iterable((busy1, Position(1, 1)))
+
+        val placement = Placement(busy1, Position(1, 1))        
+        val busyRobots = Iterable(placement)
 
         val result = selector.choose(mission, busyRobots)
 
         result shouldBe None
+
+    "evaluating" should:
+      val robot1 = Robot(RobotId("Robot1"))
+      val robot2 = Robot(RobotId("Robot2"))
+      val placement1 = Placement(robot1, Position(1, 1))
+      val placement2 = Placement(robot2, Position(2, 2))
+
+      val robots = Iterable(placement1, placement2)
+
+      "return the available robot to the mission position" in:
+
+        val result = selector.choose(mission, robots)
+
+        result shouldBe Some(placement1)
