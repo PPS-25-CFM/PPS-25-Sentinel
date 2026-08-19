4ca70da9aac76c3c5ef2cc78354975a3717d1641
Leonardo Marcaccio <leonardo.marcaccio@studio.unibo.it>
2026-08-19 20:33:04 +0200

    feat: Updated test to be able to use BeforeEach to avoid boilerplate code duplication

--- src/test/scala/it/unibo/sentinel/core/simulation/EnvironmentSpec.scala
+++ src/test/scala/it/unibo/sentinel/core/simulation/EnvironmentSpec.scala
@@ -1,180 +1,165 @@
 package it.unibo.sentinel.core.simulation
 
 import it.unibo.sentinel.UnitTest
-import it.unibo.sentinel.core.warehouse.Warehouse
-import it.unibo.sentinel.core.warehouse.Position
-import it.unibo.sentinel.core.robot.Robot
-import it.unibo.sentinel.core.robot.RobotId
-import it.unibo.sentinel.core.scenario.Placement
-import it.unibo.sentinel.core.mission.MissionId
-import it.unibo.sentinel.core.mission.Mission
-import it.unibo.sentinel.core.mission.Task
-import it.unibo.sentinel.core.mission.MissionStatus
-import it.unibo.sentinel.core.robot.RobotStatus
+import it.unibo.sentinel.core.mission.{Mission, MissionId, MissionStatus, Task}
+import it.unibo.sentinel.core.robot.{RobotId, RobotStatus}
 import it.unibo.sentinel.core.routing.Path
+import it.unibo.sentinel.core.scenario.{Scenario, Spawn}
+import it.unibo.sentinel.core.warehouse.{Area, Position, Tile, Warehouse}
+import org.scalatest.BeforeAndAfterEach
 
-class EnvironmentSpec extends UnitTest:
-  trait Fixture:
-    val pos1 = Position(1, 1)
-    val pos2 = Position(2, 2)
+import scala.compiletime.uninitialized
 
-    val r_id1 = RobotId("R1")
-    val r_id2 = RobotId("R2")
+trait EnvironmentFixture:
+  self: UnitTest =>
 
-    val bot1 = Robot(r_id1)
-    val bot2 = Robot(r_id2)
+  val width = 5
+  val r_id1: RobotId = RobotId("R1")
+  val r_id2: RobotId = RobotId("R2")
+  val m_id1: MissionId = MissionId("M1")
+  val m_id2: MissionId = MissionId("M2")
 
-    val place1 = Placement(bot1, pos1)
-    val place2 = Placement(bot2, pos2)
+  val pos1: Position = Position(0, 0)
+  val pos2: Position = Position(2, 2)
 
-    val des1 = Position(3, 3)
-    val des2 = Position(4, 4)
-
-    val task1 = Task.goto(des1)
-    val task2 = Task.goto(des2)
-
-    val m_id1 = MissionId("M1")
-    val m_id2 = MissionId("M2")
-
-    val duration = 10
-
-    val mission1 = Mission(m_id1, task1, duration)
-    val mission2 = Mission(m_id2, task2, duration)
-
-    val width = 5
-    val height = width
-
-    val warehouse = Warehouse.empty(width, height)
-    val fleet = Map[RobotId, Placement](
-      (r_id1, place1),
-      (r_id2, place2)
+  val warehouse: Warehouse = Warehouse
+    .empty(width, width)
+    .withArea(Area(Position(0, 0), Position(width - 1, width - 1)))(
+      Tile.Floor()
     )
-    val board = Map[MissionId, Mission](
-      (m_id1, mission1),
-      (m_id2, mission2)
-    )
+
+  val scenario: Scenario = (for
+    s0 <- Right(Scenario.in(warehouse))
+    s1 <- s0.place(Spawn(r_id1, pos1))
+    s2 <- s1.place(Spawn(r_id2, pos2))
+    s3 <- s2.load(Mission(m_id1, Task.goto(Position(0, 1)), 10))
+    s4 <- s3.load(Mission(m_id2, Task.goto(Position(4, 4)), 10))
+  yield s4).value
+
+/** We suppressed null warning due to the ScalaTest lifecycle `uninitialized` var usage in beforeEach.
+  */
+@SuppressWarnings(Array("org.wartremover.warts.Null"))
+class EnvironmentSpec
+    extends UnitTest
+    with BeforeAndAfterEach
+    with EnvironmentFixture:
+
+  var environment: Environment = uninitialized
+
+  override def beforeEach(): Unit =
+    super.beforeEach()
+    environment = scenario.build
 
   "An Environment" when:
 
     "initialized" should:
 
-      "have its parameters correctly set" in new Fixture:
-        val env = Environment(warehouse, fleet, board)
-
-        env.warehouse shouldBe warehouse
-        env.placements shouldBe fleet.values.toSeq
-        env.missions shouldBe board.values.toSeq
+      "have its parameters correctly set" in:
+        environment.warehouse shouldBe warehouse
+        environment.placements.map(_.robot.id) should contain theSameElementsAs Seq(r_id1, r_id2)
+        environment.missions.map(_.id) should contain theSameElementsAs Seq(m_id1, m_id2)
 
     "assigning a mission" should:
 
-      "update the robot and board when IDs exist" in new Fixture:
-        val env = Environment(warehouse, fleet, board)
-        env.assign(r_id1, m_id1)
+      "update the robot and board when IDs exist" in:
+        environment.assign(r_id1, m_id1)
 
-        val assignedMission = env.missions.find(_.id == m_id1).value
-        
+        val assignedMission = environment.missions.find(_.id == m_id1).value
         assignedMission.carrier shouldBe Some(r_id1)
         assignedMission.status shouldBe MissionStatus.Assigned
 
-        val assignedRobot = env.placements.map(_.robot).find(_.id == r_id1).value
-
+        val assignedRobot = environment.placements.map(_.robot).find(_.id == r_id1).value
         assignedRobot.status shouldBe RobotStatus.Ready
         assignedRobot.mission.value shouldBe m_id1
 
-      "do nothing if the robot ID does not exist" in new Fixture:
-        val env = Environment(warehouse, fleet, board)
-        env.assign(RobotId("UNKNOWN"), m_id1)
+      "do nothing if the robot ID does not exist" in:
+        val initialPlacements = environment.placements
+        val initialMissions = environment.missions
 
-        env.placements shouldBe fleet.values.toSeq
-        env.missions shouldBe board.values.toSeq
+        environment.assign(RobotId("UNKNOWN"), m_id1)
 
-      "do nothing if the mission ID does not exist" in new Fixture:
-        val env = Environment(warehouse, fleet, board)
-        env.assign(r_id1, MissionId("UNKNOWN"))
+        environment.placements shouldBe initialPlacements
+        environment.missions shouldBe initialMissions
 
-        env.placements shouldBe fleet.values.toSeq
-        env.missions shouldBe board.values.toSeq
+      "do nothing if the mission ID does not exist" in:
+        val initialPlacements = environment.placements
+        val initialMissions = environment.missions
+
+        environment.assign(r_id1, MissionId("UNKNOWN"))
+
+        environment.placements shouldBe initialPlacements
+        environment.missions shouldBe initialMissions
 
     "routing a robot" should:
 
-      "assign to the robot the path, if it exists in fleet" in new Fixture:
-        val env = Environment(warehouse, fleet, board)
+      "assign to the robot the path, if it exists in fleet" in:
         val path: Path = Seq.empty
-        
-        env.route(r_id1, path)
+        environment.route(r_id1, path)
 
-        val routedBot = env.placements.find(_.robot.id == r_id1).value.robot
-
+        val routedBot = environment.placements.find(_.robot.id == r_id1).value.robot
         routedBot.path shouldBe Some(path)
 
-      "do nothing if the robot ID does not exist" in new Fixture:
-        val env = Environment(warehouse, fleet, board)
+      "do nothing if the robot ID does not exist" in:
+        val initialPlacements = environment.placements
+        val initialMissions = environment.missions
         val path: Path = Seq.empty
 
-        env.route(RobotId("UNKNOWN"), path)
+        environment.route(RobotId("UNKNOWN"), path)
 
-        env.placements shouldBe fleet.values.toSeq
-        env.missions shouldBe board.values.toSeq        
+        environment.placements shouldBe initialPlacements
+        environment.missions shouldBe initialMissions
 
     "advancing a robot" should:
 
-      "update placement and step the robot if target position is free" in new Fixture:
-        val testEnv = Environment(warehouse, fleet, board)
-        val target = Position(1, 2)
+      "update placement and step the robot if target position is free" in:
+        val target = Position(0, 1)
         val path: Path = Seq(target)
-        
-        testEnv.route(r_id1, path)
-        testEnv.advance(r_id1)
 
-        val updatedPlacement = testEnv.placements.find(_.robot.id == r_id1).value
-        
+        environment.route(r_id1, path)
+        environment.advance(r_id1)
+
+        val updatedPlacement = environment.placements.find(_.robot.id == r_id1).value
         updatedPlacement.at shouldBe target
 
-      "prevent movement if the target position is occupied by another robot" in new Fixture:
-        val env = Environment(warehouse, fleet, board)
-        val collisionPath: Path = Seq(pos1, pos2)
+      "prevent movement if the target position is occupied by another robot" in:
+        // pos2 is occupied by r_id2 at Position(2, 2)
+        val collisionPath: Path = Seq(pos2)
 
-        env.route(r_id1, collisionPath)
-        env.advance(r_id1)
+        environment.route(r_id1, collisionPath)
+        environment.advance(r_id1)
 
-        val placementAfterCollision = env.placements.find(_.robot.id == r_id1).value
-        
+        val placementAfterCollision = environment.placements.find(_.robot.id == r_id1).value
         placementAfterCollision.at shouldBe pos1
 
-      "advance step-by-step through a Path" in new Fixture:
-        val env = Environment(warehouse, fleet, board)
-        val step1 = Position(1, 2)
-        val step2 = Position(1, 3)
+      "advance step-by-step through a Path" in:
+        val step1 = Position(0, 1)
+        val step2 = Position(0, 2)
         val path: Path = Seq(step1, step2)
 
-        env.route(r_id1, path)
-        
-        env.advance(r_id1)
-        env.placements.find(_.robot.id == r_id1).value.at shouldBe step1
+        environment.route(r_id1, path)
 
-        env.advance(r_id1)
-        env.placements.find(_.robot.id == r_id1).value.at shouldBe step2
+        environment.advance(r_id1)
+        environment.placements.find(_.robot.id == r_id1).value.at shouldBe step1
+
+        environment.advance(r_id1)
+        environment.placements.find(_.robot.id == r_id1).value.at shouldBe step2
 
     "queried about Standings" should:
 
-      "return Placements matching a specific Robot status" in new Fixture:
-        val env = Environment(warehouse, fleet, board)
-        
-        env.standing(RobotStatus.Idle) should contain theSameElementsAs Seq(place1, place2)
-        env.standing(RobotStatus.Ready) shouldBe empty
+      "return Placements matching a specific Robot status" in:
+        environment.standing(RobotStatus.Idle).map(_.robot.id) should contain theSameElementsAs Seq(r_id1, r_id2)
+        environment.standing(RobotStatus.Ready) shouldBe empty
 
-        env.assign(r_id1, m_id1)
-        
-        env.standing(RobotStatus.Ready).map(_.robot.id) should contain theSameElementsAs Seq(r_id1)
-        env.standing(RobotStatus.Idle).map(_.robot.id) should contain theSameElementsAs Seq(r_id2)
+        environment.assign(r_id1, m_id1)
+
+        environment.standing(RobotStatus.Ready).map(_.robot.id) should contain theSameElementsAs Seq(r_id1)
+        environment.standing(RobotStatus.Idle).map(_.robot.id) should contain theSameElementsAs Seq(r_id2)
 
     "queried about Pending Missions" should:
 
-      "return only missions in Pending status with pendingMissions" in new Fixture:
-        val env = Environment(warehouse, fleet, board)
-        
-        env.pendingMissions should contain theSameElementsAs Seq(mission1, mission2)
+      "return only missions in Pending status with pendingMissions" in:
+        environment.pendingMissions.map(_.id) should contain theSameElementsAs Seq(m_id1, m_id2)
 
-        env.assign(r_id1, m_id1)
-        
-        env.pendingMissions should contain theSameElementsAs Seq(mission2)
+        environment.assign(r_id1, m_id1)
+
+        environment.pendingMissions.map(_.id) should contain theSameElementsAs Seq(m_id2)
