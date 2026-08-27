62ab48ab665235bf28d6bf9ffe5ec0ecdeb0f225
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-08-27 13:35:43 +0200

    docs: adding scaladoc

--- src/main/scala/it/unibo/sentinel/core/routing/Cost.scala
+++ src/main/scala/it/unibo/sentinel/core/routing/Cost.scala
@@ -6,35 +6,37 @@ opaque type Cost = Double
 
 object Cost:
   /** @param value
+    *   a non-negative value representing the cost.
     * @return
+    *   a [[Cost]] built from the given value.
+    * @throws IllegalArgumentException
+    *   if the value is negative.
     */
   def apply(value: Double): Cost =
     require(value >= 0)
     value
 
   /** @return
+    *   The zero cost.
     */
   val zero: Cost = Cost(0.0)
 
   /** @return
+    *   The unit cost.
     */
   val unit: Cost = Cost(1.0)
 
   extension (cost: Cost)
     /** @return
+      *   A [[Double]] representing the value of the [[Cost]].
       */
     def value: Double = cost
 
     /** @param other
+      *   The other [[Cost]] to add.
       * @return
+      *   The sum of this [[Cost]] and the other [[Cost]].
       */
     def +(other: Cost): Cost = cost + other
-
-    /** @param weight
-      * @return
-      */
-    def *(weight: Double): Cost =
-      require(weight >= 0)
-      Cost(cost * weight)
 
   given Ordering[Cost] = Ordering.Double.TotalOrdering
