8995251782f70e77e057f167f5b0669ef073b897
Mattia <mattia.forti2@studio.unibo.it>
2026-09-05 19:01:59 +0200

    refactor: moved canMove method into CollisionChecker

--- src/main/scala/it/unibo/sentinel/core/collisions/CollisionChecker.scala
+++ src/main/scala/it/unibo/sentinel/core/collisions/CollisionChecker.scala
@@ -2,6 +2,9 @@ package it.unibo.sentinel.core.collisions
 
 import it.unibo.sentinel.core.robot.RobotId
 import it.unibo.sentinel.core.scenario.Intent
+import it.unibo.sentinel.core.scenario.Placement
+import it.unibo.sentinel.core.robot.RobotStatus
+import it.unibo.sentinel.core.simulation.Tick
 
 /** Used to check for collisions between [[Robot]]s
   */
@@ -15,6 +18,15 @@ trait CollisionChecker:
     */
   def checkCollisions(intents: Seq[Intent]): Seq[Seq[RobotId]]
 
+  /** @param placement
+    *   the placement of the robot to check
+    * @param placements
+    *   group of placements to check against
+    * @return
+    *   `true` if the robot can move to its intended position, `false` otherwise
+    */
+  def canMove(placement: Placement, placements: Seq[Placement]): Boolean
+
 object CollisionChecker extends CollisionChecker:
 
   override def checkCollisions(intents: Seq[Intent]): Seq[Seq[RobotId]] =
@@ -22,3 +34,14 @@ object CollisionChecker extends CollisionChecker:
       .groupBy(_.position)
       .map(_._2.map(_.robotId))
       .toSeq
+
+  override def canMove(placement: Placement, fleet: Seq[Placement]): Boolean =
+    placement.robot.status == RobotStatus.Moving &&
+      fleet.filterNot(_ == placement).forall { other =>
+        val targetOccupied = other.at == placement.intent.position
+        lazy val targetBlocked =
+          other.robot.remaining == Tick.zero ||
+            other.intent.position == placement.at ||
+            !canMove(other, fleet)
+        !(targetOccupied && targetBlocked)
+      }
--- src/main/scala/it/unibo/sentinel/core/collisions/PauseCollisionHandler.scala
+++ src/main/scala/it/unibo/sentinel/core/collisions/PauseCollisionHandler.scala
@@ -3,7 +3,6 @@ package it.unibo.sentinel.core.collisions
 import it.unibo.sentinel.core.scenario.Placement
 import it.unibo.sentinel.core.simulation.Event
 import it.unibo.sentinel.core.robot.RobotStatus
-import it.unibo.sentinel.core.simulation.Tick
 
 private[collisions] final class PauseCollisionHandler extends BasicHandler:
 
@@ -12,7 +11,8 @@ private[collisions] final class PauseCollisionHandler extends BasicHandler:
   ): Seq[Event] =
     val directCollisions = pairCollisions(placements)
     val directEvents = pauseMoving(directCollisions)
-    val unmovable = placements.filterNot(canMove(_, placements))
+    val unmovable =
+      placements.filterNot(CollisionChecker.canMove(_, placements))
     val unmovableEvents = pauseMoving(unmovable)
     val movable = placements.filterNot(unmovable.toSet)
     val groupEvents = CollisionChecker
@@ -61,15 +61,3 @@ private[collisions] final class PauseCollisionHandler extends BasicHandler:
         other.intent.position == p.at
       }
     }
-
-  private def canMove(placement: Placement, fleet: Seq[Placement]): Boolean =
-    placement.robot.status == RobotStatus.Moving &&
-      fleet.filterNot(_ == placement).forall { other =>
-        val targetOccupied = other.at == placement.intent.position
-        lazy val targetBlocked =
-          other.robot.remaining == Tick.zero ||
-            other.intent.position == placement.at ||
-            !canMove(other, fleet)
-
-        !(targetOccupied && targetBlocked)
-      }
