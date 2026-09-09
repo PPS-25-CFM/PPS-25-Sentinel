31d954af55999cb5d1aa57424c870c3888ea98ba
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-09-09 10:11:49 +0200

    feat: obstacles metric

--- src/main/scala/it/unibo/sentinel/core/routing/Metric.scala
+++ src/main/scala/it/unibo/sentinel/core/routing/Metric.scala
@@ -27,3 +27,12 @@ object Metric:
   object Time extends Metric:
     override def cost(to: Position)(using warehouse: Warehouse): Option[Score] =
       for tick <- warehouse.traversalCost(to) yield Score(tick.value)
+
+  /** Assigns a [[Score]] to every traversed position based on the number of
+    * obstacles in the way.
+    */
+  object Obstacles extends Metric:
+    override def cost(to: Position)(using warehouse: Warehouse): Option[Score] =
+      val nearbyObstacles =
+        warehouse.neighbors(to).size - warehouse.traversableNeighbors(to).size
+      Option.when(warehouse.isTraversable(to))(Score(1 + nearbyObstacles))
