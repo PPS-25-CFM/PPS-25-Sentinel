c1d491e96fca6d17e0fa46efdeb3e6f14b78971c
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-09-15 14:08:44 +0200

    refactor: no need of _

--- src/test/scala/it/unibo/sentinel/core/simulation/BasicSimulationSpec.scala
+++ src/test/scala/it/unibo/sentinel/core/simulation/BasicSimulationSpec.scala
@@ -5,9 +5,10 @@ import it.unibo.sentinel.UnitTest
 class BasicSimulationSpec extends UnitTest with SimulationBehaviours:
   "A BasicSimulation" when:
 
-    behave like commonSimulation(Simulation.of(_))
+    behave like commonSimulation(Simulation.of)
 
     "when there are still missions" should:
       val sim = Simulation.of(scenario)
+
       "not be over" in:
         sim.isOver shouldBe false
